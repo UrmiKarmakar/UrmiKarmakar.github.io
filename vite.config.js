55d51cb4ea585941ebd1f2dde3095e494cd93bc5
Urmi Karmakar <urmi16kk@gmail.com>
2026-04-02 11:02:29 +0600

    Fix some issues

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Change this to '/' for the most stable deployment on your main github.io domain
   base: '/', 
-  build: {
-    outDir: 'docs', // This tells Vite to put the build in 'docs'
-  },
   resolve: {
-    alias: { "@": path.resolve(__dirname, "./src") },
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+  // This helps prevent the "white screen" if assets are large
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  logLevel: 'info', 
 })
